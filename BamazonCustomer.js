30e034d24a49b6f05217784b058b389b4d538318
Vis Vasanth <viscodesdpt@gmail.com>
2016-10-21 18:15:22 -0500

    working if else

--- BamazonCustomer.js
+++ BamazonCustomer.js
@@ -3,10 +3,23 @@
 var mysql = require('mysql');
 var inquirer = require('inquirer');
 
+
+var logo = 
+        "      • ˚ •˛•˚ * 。 • ˚ ˚ ˛ ˚ ˛ •\n"+
+        "      • ˚B AMAZON ★* 。 • ˚ ˚ ˛ ˚ ˛ •\n"+
+        "      •。★Shop from Home!★ 。* • ˚。\n"+
+        "      ° 。 ° ˛˚˛ *_Π_____*。*˚\n"+
+        "      ˚ ˛ •˛•˚ */______/~＼。˚ ˚ ˛\n"+
+        "      ˚ ˛ •˛• ˚｜ 田田｜門｜ ˚\n";
+
+
+
+
 var connection = mysql.createConnection({
     host: "localhost",
     port: 3306,
-
+    user: "newuser", //Your username
+    password: "password", //Your password
     database: "Bamazon"
 })
 
@@ -18,18 +31,19 @@ connection.connect(function(err) {
     
 })
 
+
+console.log(logo);
+
 function displayItems(){
 connection.query('SELECT * FROM Products', function(err, res){
 	for (var i =0; i<res.length; i++){
-		console.log(res[i].ItemID + " | " + res [i].ProductName + "|" + "$" +res[i].Price + "|");
+        
+		console.log(res[i].ItemID + " | " + res [i].ProductName + " | " + "$" +res[i].Price + " | ");
 	}
 	console.log("============================");
 	productSearch();
 })
 }
-
-// The first should ask them the ID of the product they would like to buy.
-// The second message should ask how many units of the product they would like to buy.
 
 var productSearch = function() {
     inquirer.prompt([{
@@ -37,42 +51,50 @@ var productSearch = function() {
         type: "input",
         message: "What product are you looking for? ",
      
-    }]).then(function(answer) {
+      },
+      {
+     name: "howMany",
+     type: "input",
+     message: "How many would you like to purchase?",
+        validate: function(value) {
+            if (isNaN(value) == false) {
+                return true;
+            } else {
+                return false;
+            }
+        }
+ }]).then(function(answer) {
         var query = 'SELECT ProductName,DepartmentName,Price,StockQuantity FROM Products WHERE ?';
         connection.query(query, {ProductName: answer.searchProduct}, function(err, res) {
             for (var i = 0; i < res.length; i++) {
                 console.log(res [i].ProductName + "  |  " + res[i].DepartmentName + "  |  " + "$" +res[i].Price + " | " + res[i].StockQuantity + "  |  " );
-            }
-        });
-    });
-    productQuantity();
-};
+                    
+                if (answer.StockQuantity < res[i].StockQuantity) {
+                    console.log("Great! We have that amount!");
+                } 
+                else{
+                    console.log("SORRY! Insufficient quantity! (Order has not gone through)");
+                }  
+                // switch(true){
+                //     case (answer.StockQuantity<res[i].StockQuantity): 
+                //         console.log("Great! We have that amount!");
+                //         break;
 
-var productQuantity = function(){
-	inquirer.prompt([{
-		name: "howMany",
-		type: "input",
-		search: "How many would you like to purchase?",
-	}]).then(function(answer){
-		var query = 'SELECT ProductName, Price, StockQuantity FROM Products WHERE ?';
-		connection.query(query, {StockQuantity: answer.howMany}, function(err, res) {
-            for (var i = 0; i < res.length; i++) {
-                console.log(res [i].ProductName + "  |  " + "$" +res[i].Price + " | " + res[i].StockQuantity + "  |  " );
-            }
-        });
+                //     case (answer.StockQuantity>res[i].StockQuantity):
+                //         console.log("SORRY! WE ARE SOLD OUT!");
+                //         break;
 
-	});
-};
-
-
-
-
-
-
-// Once the customer has placed the order, your application should check if your store has enough of the product to meet the customer's request.
-
-// If not, the app should log a phrase like Insufficient quantity!, and then prevent the order from going through.
-// However, if your store does have enough of the product, you should fulfill the customer's order.
+                // }
+                
+//                 However, if your store does have enough of the product, you should fulfill the customer's order.
 
 // This means updating the SQL database to reflect the remaining quantity.
 // Once the update goes through, show the customer the total cost of their purchase.
+            }
+
+
+        });
+        
+    });
+    
+};
